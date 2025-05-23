097f6379c8ac8542cf1337daf5102b4a259d6754
Tushar <tusharjain2308@gmail.com>
2025-05-23 14:40:31 +0530

    AWS Elastic beanstalk Hosting

--- frontend/src/utils/apiPaths.js
+++ frontend/src/utils/apiPaths.js
@@ -1,4 +1,4 @@
-export const BASE_URL="https://hashtag-generator-backend.onrender.com"
+export const BASE_URL="http://hashpopbackend-env.eba-phdktvkm.ap-south-1.elasticbeanstalk.com"
 
 export const API_PATHS = {
     AUTH : {
